3d95f27a0e7805ea6ddc393dcbb0f6711afc2a49
Vitaly Takmazov <vitalyster@gmail.com>
2015-04-24 15:29:11 +0300

    imap backend

---
+++ spectrum2_imap_backend/IMAPPlugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using ARSoft.Tools.Net.Dns;
+using networkplugin_csharp;
+using pbnetwork;
+using S22.Imap;
+
+namespace spectrum2_imap_backend
+{
+    public class IMAPPlugin : NetworkPlugin
+    {
+        readonly Dictionary<string, ImapClient> _sessions; 
+
+        public IMAPPlugin(string host, string port) : base(host, port)
+        {
+            _sessions = new Dictionary<string, ImapClient>();
+            
+            LoggedIn += (sender, login) =>
+            {
+                var address = new MailAddress(login.LoginPayload.legacyName);
+                var service = DnsClient.Default.Resolve(address.Host, RecordType.Srv)
+                    .AnswerRecords.OfType<SrvRecord>().First();
+                var newClient = new ImapClient(service.Target, service.Port, 
+                    login.LoginPayload.legacyName, login.LoginPayload.password, AuthMethod.Auto, true);
+                SendMessage(WrapperMessage.Type.TYPE_BUDDY_CHANGED, new Buddy
+                {
+                    userName = login.LoginPayload.user,
+                    buddyName = address.Host,
+                    status = StatusType.STATUS_ONLINE
+                });
+                newClient.NewMessage += (o, args) =>
+                {
+                    var msg = args.Client.GetMessage(args.MessageUID, FetchOptions.HeadersOnly).Subject;
+                    SendMessage(WrapperMessage.Type.TYPE_CONV_MESSAGE, new ConversationMessage
+                    {
+                        userName = login.LoginPayload.user,
+                        buddyName = address.Host,
+                        headline = true,
+                        message = msg
+                    });
+                };
+                _sessions.Add(login.LoginPayload.user, newClient);
+            };
+
+            LoggedOut += (sender, args) =>
+            {
+                var user = args.LogoutPayload.user;
+                if (!_sessions.ContainsKey(user)) return;
+                _sessions[user].Logout();
+                _sessions.Remove(user);
+            };
+        }
+    }
+}
---
+++ spectrum2_imap_backend/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using ARSoft.Tools.Net.Dns;
+
+namespace spectrum2_imap_backend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new ConsoleTraceListener(true));
+            var host = args[1];
+            var port = args[3];
+            var backend = new IMAPPlugin(host, port);
+            backend.Connect();
+            backend.Loop();
+        }
+    }
+}
